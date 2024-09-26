3f8a010395a35a764b62535327ae06acabccf0c2
MichaelLELU <lelu.michael@gmail.com>
2024-09-26 17:06:55 +0200

    fix: error

--- back/src/index.ts
+++ back/src/index.ts
@@ -12,24 +12,36 @@ const PORT = 3000;
 
 app.use(express.json());
 
+// READ ONE AD
+
 app.get("/ads/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   try {
     const ad = await Ad.findOneBy({ id });
+    if (!ad) {
+      res.status(404).send("Ad not found");
+    }
     res.send(ad);
   } catch (err) {
-    res.status(404).send("Ad not found");
+    res.status(500).send("Ad not found");
   }
 });
+
+// READ ALL ADS
 
 app.get("/ads", async (req, res) => {
   try {
     const ads = await Ad.find();
+    if (!ads) {
+      res.status(404).send("No ads found");
+    }
     res.send(ads);
   } catch (err) {
-    res.status(404).send("No ads found");
+    res.status(500).send("No ads found");
   }
 });
+
+// CREATE AN AD
 
 app.post("/ads", (req, res) => {
   const { title, description, owner, price, picture, location } = req.body;
@@ -46,13 +58,18 @@ app.post("/ads", (req, res) => {
 
     res.send(add).status(201);
   } catch (err) {
-    res.status(400).send("Bad request");
+    res.status(500).send("Bad request");
   }
 });
+
+// DELETE AN AD
 
 app.delete("/ads/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   try {
+    if (!id) {
+      res.status(204).send("Ad not found");
+    }
     await Ad.delete({ id });
     res.send("OK Ad deleted").status(205);
   } catch (err) {
