39cf648c892ce44762c2752762a312ae40152a60
MichaelLELU <lelu.michael@gmail.com>
2024-09-30 15:35:57 +0200

    fix: detail page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
-import Ads from "./components/Ads";
 
 function App() {
   return (
-    <body>
+    <>
       <Header />
-      <Ads />
-    </body>
+      <Outlet />
+    </>
   );
 }
 
