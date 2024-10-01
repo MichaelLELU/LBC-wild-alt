0c411fa1c99e47899fa59c08371379cb3f031f62
MichaelLELU <lelu.michael@gmail.com>
2024-10-01 12:47:05 +0200

    feat: state

--- front/src/components/Ads.tsx
+++ front/src/components/Ads.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import AdCard from "./AdCard";
 import data from "../../public/data/data.json";
 
 export default function Ads() {
+  const [total, setTotal] = useState(0);
+
   return (
     <main className="main-content">
       <h2>Annonces récentes</h2>
+      <p>prix : {total} €</p>
       <section className="recent-ads">
         {data.map((ads, i) => (
-          <AdCard key={i} data={ads} />
+          <div key={i}>
+            <AdCard data={ads} />
+            <button
+              className="button"
+              onClick={() => {
+                setTotal(total + ads.price);
+              }}
+            >
+              Ajouter au panier
+            </button>
+          </div>
         ))}
       </section>
     </main>
